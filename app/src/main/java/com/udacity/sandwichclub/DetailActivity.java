e2f5f6cc92bd5325c05fedd9a4646f7719aa3fe3
jgebbeken <josh.gebbeken@asu.edu>
2018-04-25 20:12:07 -0700

    Project tested and working as expected. Fix text in AKA and Description TextViews going passed the screen. Changed how the constraints operated these controls. Fixed warnings that made my if statements always true.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -21,6 +24,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +47,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +60,59 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+        // Setup the TextViews
+        TextView tvDescription = findViewById(R.id.description_tv);
+        TextView tvOrigin = findViewById(R.id.origin_tv);
+        TextView tvAka = findViewById(R.id.also_known_tv);
+        TextView tvIngredients = findViewById(R.id.ingredients_tv);
+
+        // Set the TextViews
+        tvDescription.setText(sandwich.getDescription());
+        tvOrigin.setText(sandwich.getPlaceOfOrigin());
+
+        // Need a List of String for the AKA.
+        List<String> akaList =  sandwich.getAlsoKnownAs();
+
+        // Get a StringBuilder object to start extracting the List of Strings
+        StringBuilder extractedAKAList = new StringBuilder();
+
+        for(int i = 0; i < akaList.size(); i++) {
+
+            extractedAKAList.append(akaList.get(i));
+
+            // If the sandwich has multiple AKAs add commas and spacing but only if it's not the
+            // end
+            if(i < akaList.size() -1){
+                extractedAKAList.append(", ");
+            }
+
+        }
+
+        // Once AKA list is completed extracting in the StringBuilder set text in UI.
+        tvAka.setText(extractedAKAList.toString());
+
+        // Create a new StringBuilder object for the ingredients
+        StringBuilder extractedIngredients = new StringBuilder();
+
+        // Create a new List of Strings for ingredients.
+        List<String> ingredientsList = sandwich.getIngredients();
+
+        for(int i = 0; i < ingredientsList.size(); i++ ) {
+
+            extractedIngredients.append(ingredientsList.get(i));
+
+            // If there are multiple ingredients in the list. Put it on the next line.
+            if(i < ingredientsList.size() -1) {
+                extractedIngredients.append("\n");
+            }
+        }
+
+        // Once the StringBuilder operation is complete place it in the UI.
+        tvIngredients.setText(extractedIngredients.toString());
+
+
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,88 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+
+        //Create the sandwich object
+        Sandwich sandwich = new Sandwich();
+
+        // In the JSON the object has an JSON array called ingredients. We need a list to store this
+        // Data. The AKA has another JSON array as well. So another list is needed.
+        List<String> ingredientsList = new ArrayList<>();
+        List<String> alsoKnownAsList = new ArrayList<>();
+
+
+
+        //This JSON has an object within an object so we get the "name" JSON object.
+        try {
+            JSONObject sandwiches = new JSONObject(json).getJSONObject("name");
+
+            //Set the main name
+            sandwich.setMainName(sandwiches.getString("mainName"));
+
+
+
+            // The array has a list of Strings for the AKA. We don't know how many there is so we
+            // have to grab each of them if any.
+            JSONArray sandwichArray = sandwiches.getJSONArray("alsoKnownAs");
+
+            for(int i = 0; i < sandwichArray.length(); i++) {
+
+                // Add item in the Object array.
+                alsoKnownAsList.add(sandwichArray.getString(i));
+            }
+
+            // Checking to see if any data was added to the list. Add only if there is data.
+            if(!alsoKnownAsList.isEmpty()) {
+                sandwich.setAlsoKnownAs(alsoKnownAsList);
+            }
+
+
+            // We were done with object "name". Going back to the first object
+            sandwiches = new JSONObject(json);
+
+            //Set each item using the sandwich setter.
+            sandwich.setPlaceOfOrigin(sandwiches.getString("placeOfOrigin"));
+            sandwich.setDescription(sandwiches.getString("description"));
+            sandwich.setImage(sandwiches.getString("image"));
+
+
+            //Create the JSON ingredients array
+            JSONArray sandwichIngredients = sandwiches.getJSONArray("ingredients");
+
+            // Since we don't know how many ingredients will be in the array. We need loop in till
+            // we have all of it.
+            for(int i = 0; i < sandwichIngredients.length(); i++){
+
+                ingredientsList.add(sandwichIngredients.getString(i));
+            }
+
+            // Make sure there are any ingredients in the list before proceeding
+            if(!ingredientsList.isEmpty()) {
+                sandwich.setIngredients(ingredientsList);
+            }
+
+
+        } catch (JSONException e) {
+            // Catch any errors if JSON caused an error
+            e.printStackTrace();
+        }
+
+
+        return sandwich;
     }
 }
